3c0b7b1008b0a3c93a9e2878cff86ed3bdf4e3d2
Alan Hunter <localpunter@sky.com>
2018-08-14 16:46:36 +0100

    D2 Lab final update

--- day_2/D2lab/spacecowboys/console.rb
+++ day_2/D2lab/spacecowboys/console.rb
@@ -1,2 +1,30 @@
 require ('pry-byebug')
 require_relative('./models/bounties.rb')
+
+bounty1 = Bounties.new({
+  "name" => "Davros",
+  "species" => "Dalek",
+  "bounty_value" => 100,
+  "fav_weapon" => "Exterminator"
+  })
+
+  bounty2 = Bounties.new({
+  "name" => "Darth Maul",
+  "species" => "Sith",
+  "bounty_value" => 80,
+  "fav_weapon" => "Double Bladed Lightsaber"
+    })
+
+  bounty3 = Bounties.new({
+  "name" => "Joker",
+  "species" => "Human?",
+  "bounty_value" => 50,
+  "fav_weapon" => "Practical Jokes"
+    })
+
+bounty1.save()
+bounty2.save()
+bounty1.delete()
+bounty2.bounty_value = 70
+bounty2.update
+p bounty2.find_by_name()
--- day_2/D2lab/spacecowboys/models/bounties.rb
+++ day_2/D2lab/spacecowboys/models/bounties.rb
@@ -13,4 +13,51 @@ attr_accessor(:name, :species, :bounty_value, :fav_weapon)
     @fav_weapon = options["fav_weapon"]
 
   end
+  def save()
+    db = PG.connect({dbname: 'spacecowboys', host: 'localhost'})
+    sql = "INSERT INTO bounties (name, species, bounty_value, fav_weapon)
+    VALUES
+    ($1, $2, $3, $4) RETURNING *"
+    values = [@name, @species, @bounty_value, @fav_weapon]
+    db.prepare("save", sql)
+    @id = db.exec_prepared("save", values)[0]["id"].to_i()
+    db.close()
+  end
+
+  def update()
+    db = PG.connect({dbname: 'spacecowboys', host: 'localhost'})
+    sql = "UPDATE bounties
+    SET(name, species, bounty_value, fav_weapon)
+    = ($1, $2, $3, $4)
+    WHERE id = $5"
+    values = [@name, @species, @bounty_value, @fav_weapon, @id]
+    db.prepare("update", sql)
+    db.exec_prepared("update", values)
+    db.close
+
+  end
+
+  def delete()
+    db = PG.connect({dbname: 'spacecowboys', host: 'localhost'})
+    sql = "DELETE FROM bounties WHERE id = $1"
+    values = [@id]
+    db.prepare("delete", sql)
+    db.exec_prepared("delete", values)
+    db.close
+
+  end
+
+  def Bounty.find_by_name(name)
+    db = PG.connect({dbname: 'spacecowboys', host: 'localhost'})
+    sql = "SELECT * FROM bounties WHERE name = $1"
+    values = [name]
+    db.prepare("find_by_name", sql)
+    name_found = db.exec_prepared("find_by_name", values)
+    db.close
+    return name_found.map { |name|
+    Bounties.new(name) }
+
+  end
+
+
 end
